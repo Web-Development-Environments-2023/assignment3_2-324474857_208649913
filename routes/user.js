eb5dea773cee561d21283a7232904b908d9918d9
ArielDawidowicz <arieldawidowicz1@gmail.com>
2023-05-25 18:14:21 +0300

    my_recipes route added

--- routes/user.js
+++ routes/user.js
@@ -53,6 +53,39 @@ router.get('/favorites', async (req,res,next) => {
 });
 
 
+/**
+ * This path returns the recipes that were created by the logged-in user
+ */
+router.get('/my-recipes', async (req,res,next) => {
+  try{
+    const user_id = req.session.user_id;
+    const query = `SELECT * from my_recipes where user_id = ${user_id}`;
+    const my_recipes = await DButils.execQuery(query);
+    res.status(200).send(my_recipes);
+  } catch(error){
+    next(error); 
+  }
+});
+
+
+/* 
+Insert into my_recipes a new recipe that the looged in user created
+*/
+router.post('/my-recipes', async (req, res, next) => {
+  try {
+    const user_id = req.session.user_id;
+    const { title, image, readyInMinutes, vegetarian, vegan, glutenFree, profilePic } = req.body;
+    await DButils.execQuery(
+      `INSERT INTO my_recipes (user_id, title, image, readyInMinutes, vegetarian, vegan, glutenFree, profilePic)
+      VALUES ('${user_id}', '${title}', '${image}', '${readyInMinutes}', '${vegetarian}', '${vegan}', '${glutenFree}', '${profilePic}')`
+    );
+    res.sendStatus(201);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
 
 
 module.exports = router;
