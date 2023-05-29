bc1c58f85d9b43efb21293ea32d4e07d37d393bc
ArielDawidowicz <arieldawidowicz1@gmail.com>
2023-05-29 12:19:11 +0300

    .env file

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -22,7 +22,7 @@ async function getRandomRecipes(number) {
   return await axios.get(`${api_domain}/random`, {
     params: {
       number: number,
-      apiKey: 'e8e5d590af004de4a54b294a0aa3e81b'//process.env.spoonacular_apiKey
+      apiKey: process.env.spoonacular_apiKey
     }
   });
 }
@@ -38,7 +38,7 @@ async function searchRecipes(query, cuisine, diet, intolerances, number) {
   try {
     const response = await axios.get(`${api_domain}/complexSearch`, {
       params: {
-        apiKey: 'e8e5d590af004de4a54b294a0aa3e81b', //process.env.spoonacular_apiKey,
+        apiKey: process.env.spoonacular_apiKey,
         query: query,
         cuisine: cuisine,
         diet: diet,
