43862398d66437eac60d04bd7e0a94af05f4027e
Eitan Markman <eitanmarkman8@gmail.com>
2023-05-29 12:32:31 +0300

    search recipe with instructions

--- routes/recipes.js
+++ routes/recipes.js
@@ -21,7 +21,7 @@ router.get('/search', async (req,res,next) => {
     const search = await recipes_utils.searchRecipes(query, cuisine, diet, intolerances, number);
     const response = [];
     for(let i = 0 ;i<search.length; i++){
-      response.push(await recipes_utils.getRecipeDetails(search[i].id));
+      response.push(await recipes_utils.getFullRecipeDetails(search[i].id));
     }
     res.status(200).send(response);
   }catch(error){
